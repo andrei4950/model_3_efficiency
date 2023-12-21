0ac45e10b95d8561304672a57c37078fc4423cdd
andynic <andysnic@hotmail.com>
2023-12-21 19:09:57 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+import random
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def quadratic(x, a, b, c):
+    return a*x**2 + b*x + c
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    x = np.linspace(0, 10, 11)
+    print(x)
+    err = np.array([random.randint(1, 100)/10 for _ in range(len(x))])
+    y = quadratic(x, 1, 0, 0) + err
+    print(y)
+    plt.scatter(x, y)
 
-
-    """"parameters, covariant_matrix = curve_fit(f=fit_function, xdata=processed_times, ydata=distances, sigma=distance_err,
-                                             absolute_sigma=True, p0=p0)
+    parameters, covariant_matrix = curve_fit(f=quadratic, xdata=x, ydata=y)
     parameter_uncertainties = np.sqrt(covariant_matrix.diagonal())
     print(parameters)
-    print(parameter_uncertainties)""""
+    print(parameter_uncertainties)
+
+    plt.plot(x, quadratic(x, parameters[0], parameters[1], parameters[2]))
+    plt.show()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
